b7a75ff4bc9d6ac017c44b87bc0aff18a0abfa2e
oluwaseun <sondraycabana@gmail.com>
2020-11-17 11:27:33 +0100

    initial project set up

---
+++ src/main/java/com/petstore/peststore/PeststoreApplication.java
@@ -0,0 +1,14 @@
+package com.petstore.peststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeststoreApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PeststoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/petstore/peststore/web/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.petstore.peststore.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/welcome")
+    public  @ResponseBody  String  welcomeMessage(){
+        return "pet store  application running";
+
+    }
+    @GetMapping("/")
+    public  String displayWlecomePage(){
+        return "welcome";
+    }
+}
